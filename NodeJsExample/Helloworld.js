6c36cd08171e06f983c286b43986e08559c6604d
DhanshaVaibhav <DhanshaVaibhav@Dhanusha.mshome.net>
2019-05-28 02:06:55 +0530

    First commit

---
+++ NodeJsExample/Helloworld.js
@@ -0,0 +1,5 @@
+/**
+ * This is my first Node Js program.
+ */
+
+console.log("Hello world!!");
---
+++ NodeJsExample/server.js
@@ -0,0 +1,12 @@
+/**
+ * This is the Node JS Server File. 
+ */
+
+var http = require("http");
+var server = http.createServer(function(request, response){
+	response.writeHead(200,{"content-type":"text/html"});
+	
+	response.end("Hello world");
+	console.log("Hello people!!!");
+	}).listen(8888);
+
